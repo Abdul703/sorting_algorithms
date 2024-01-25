7377693a80cafe28ddab85571aef262dc9193e84
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-25 09:57:24 +0100

    added answer for complexity

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,3 +27,20 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
+
+/**
+ * swap_int - swap 2 integers using pointer
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int initial_a;
+
+initial_a = *a;
+*a = *b;
+*b = initial_a;
+}
